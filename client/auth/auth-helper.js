2c7bb283ea3b1dee26ee1e38a6643bbbe5c99055
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-05-21 19:02:27 +0300

    Added methods to manage auth state

---
+++ client/auth/auth-helper.js
@@ -0,0 +1,35 @@
+const authenticate = (jwt, cb) => {
+  if (typeof window !== 'undefined') {
+    sessionStorage.setItem('jwt', JSON.stringify(jwt))
+  }
+
+  cb()
+}
+
+const isAuthenticated = () => {
+  if (typeof window === 'undefined') {
+    return false
+  }
+
+  if (sessionStorage.getItem('jwt')) {
+    return JSON.parse(sessionStorage.getItem('jwt'))
+  }
+  else {
+    return false
+  }
+}
+
+const signout = (cb) => {
+  if (typeof window !== 'undefined') {
+    sessionStorage.removeItem('jwt')
+  }
+
+  cb()
+
+  signout()
+  .then((data) => {
+    document.cookie = 't=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+  })
+}
+
+export { authenticate, isAuthenticated, signout }
