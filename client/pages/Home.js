062e7263745883d1f4524e0b87f378c6c5ab1adb
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-05-21 14:55:12 +0300

    Added PropTypes validation

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -12,13 +12,17 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 5
   },
   title: {
-    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`
+    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
     color: theme.palette.text.secondary
   },
   media: {
     minHeight: 330
   }
 })
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+}
 
 class Home extends Component {
   render() {
