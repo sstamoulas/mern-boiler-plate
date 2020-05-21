3c71bf66ca7a999830352c46e0064cff241ef2d0
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-05-21 14:49:12 +0300

    Added Style Declarations using css in js

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -5,6 +5,21 @@ import Card, { CardContent, CardMedia } from 'material-ui/Card'
 import Typography from 'material-ui/styles/typography'
 import seashellImg from './../assets/images/seashell.jpg'
 
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding: `${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+})
+
 const Home = () => {
 
 }
