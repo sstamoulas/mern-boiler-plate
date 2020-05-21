e55971852a975596d924dc601bb7f896cbbe66cf
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-05-21 18:52:52 +0300

    Added Auth Sign In fetch api call

---
+++ client/auth/api-auth.js
@@ -0,0 +1,19 @@
+const signin = (user) => {
+  return fetch('/auth/signin', {
+    method: 'POST',
+    haders: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    body: JSON.stringify(user)
+  })
+  .then((response) => {
+    return response.json()
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+export { signin }
